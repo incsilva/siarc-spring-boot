815c2f1911e6e4e8eab1b7f88733c2b8a270f5bd
incsilva <snhrlbr@gmail.com>
2022-12-05 14:37:44 -0300

    estilizacao da view de funcao do usuario

--- src/main/java/br/edu/ifrn/jc/siarc/PopulacaoInicialBanco.java
+++ src/main/java/br/edu/ifrn/jc/siarc/PopulacaoInicialBanco.java
@@ -58,7 +58,7 @@ public class PopulacaoInicialBanco implements CommandLineRunner {
 		aparelhoRepo.save(a2);
 		aparelhoRepo.save(a3);
 
-		Usuario u1 = new Usuario("alberi", "silva.inacio@escolar.ifrn.edu.br", "2019106", passwordEncoder.encode("Alberi@2020"));
+		Usuario u1 = new Usuario("Alberi Inácio da Silva", "silva.inacio@escolar.ifrn.edu.br", "2019106", passwordEncoder.encode("Alberi@2020"));
 		u1.setRole(Role.ADMIN.getNome());
 
 		Usuario u2 = new Usuario("user", "user@gmail.com", "2018106", passwordEncoder.encode("Alberi@2020"));
